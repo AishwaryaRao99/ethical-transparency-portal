7513be404313395560e8803d5bcbb517f96a9f36
Aishwarya Rao <aishwaryarao569@gmail.com>
2025-11-12 20:27:47 +0530

    First commit of project

---
+++ transparency-portal/src/main/java/com/aishwarya/ethical/transparency_portal/TransparencyPortalApplication.java
@@ -0,0 +1,13 @@
+package com.aishwarya.ethical.transparency_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransparencyPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransparencyPortalApplication.class, args);
+	}
+
+}
